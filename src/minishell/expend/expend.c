46a258f21a129bea67f518e3871e3e88794bf50d
Azard444 <legrandgriffon.1000@gmail.com>
2025-02-12 17:17:58 +0100

    ft_strdup_index, permettant de mettre dans la liste chainee les bon contenu (voir test)

--- src/minishell/expend/expend.c
+++ src/minishell/expend/expend.c
@@ -6,7 +6,7 @@
 /*   By: eazard <eazard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:46:13 by eazard            #+#    #+#             */
-/*   Updated: 2025/02/12 16:34:46 by eazard           ###   ########.fr       */
+/*   Updated: 2025/02/12 17:15:36 by eazard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ void	expend(t_data *data)
 {
 	t_dll_node	*token;
 
-	ft_printf("expend IN\n\n");
 	token = data -> lexer -> linked_token -> head;
 	while (token)
 	{
@@ -30,7 +29,6 @@ void	expend(t_data *data)
 			expend_token_value(token -> content, data);
 		token = token -> next;
 	}
-	ft_printf("expend OUT\n\n");
 }
 
 //TEST : ceci est un $test "$bizzare" '$voyons voir ce qui se produit'
--- src/minishell/expend/expend_token_value.c
+++ src/minishell/expend/expend_token_value.c
@@ -6,7 +6,7 @@
 /*   By: eazard <eazard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:20:43 by eazard            #+#    #+#             */
-/*   Updated: 2025/02/12 16:35:23 by eazard           ###   ########.fr       */
+/*   Updated: 2025/02/12 17:17:15 by eazard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,44 @@ static size_t	get_str_end(char *value, size_t begin)
 	return (end);
 }
 
+static char	*ft_strdup_index(char *str, size_t begin, size_t end)
+{
+	char	*new_str;
+	size_t	i;
+
+	new_str = ft_calloc(end - begin + 1, 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (begin + i < end)
+	{
+		new_str[i] = str[begin + i];
+		i++;
+	}
+	return (new_str);
+}
+
+static void	ft_pustr(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	write(1, "->", 2);
+	while (str[i])
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+	write(1, "<-", 2);
+	write(1, "\n", 1);
+}
+
+
 void	expend_token_value(t_token_content *token_content, t_data *data)
 {
 	t_dll_list	*new_value_dll;
+	t_dll_node	*tmp_node;
+	char 		*tmp_str;
 	char		*new_value_str;
 	size_t		begin;
 	size_t		end;
@@ -60,9 +95,16 @@ void	expend_token_value(t_token_content *token_content, t_data *data)
 			end = get_dollar_end(token_content -> value, begin);
 		else
 			end = get_str_end(token_content -> value, begin);
-		ft_printf("begin : ->%c<-\nend :->%c<-\n", (token_content -> value)[begin],
-			(token_content -> value)[end]);
+		tmp_str = ft_strdup_index((token_content -> value), begin, end);
+		if (!tmp_str)
+			return (dll_clear_list(new_value_dll, &free), fatal_error_clean_exit(data, MALLOC_FAILURE));
+		tmp_node = dll_new_node(tmp_str);
+		if (!tmp_node)
+			return (free(tmp_str), dll_clear_list(new_value_dll, &free), fatal_error_clean_exit(data, MALLOC_FAILURE));
+		dll_insert_tail(new_value_dll, tmp_node);
 		begin = end;
 	}
+	dll_print_list(new_value_dll, (void (*)(void *))(&ft_pustr));
+
 	(void)new_value_str;
 }
