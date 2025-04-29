b409082fb0c32fc4065473864cbf8d12cae26596
Emile Azard <eazard@paul-f3Br7s2.clusters.42paris.fr>
2025-04-29 17:37:52 +0200

    historique rajoute

--- src/minishell/data/free_space/free_data.c
+++ src/minishell/data/free_space/free_data.c
@@ -6,7 +6,7 @@
 /*   By: eazard <eazard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 16:25:00 by cboma-ya          #+#    #+#             */
-/*   Updated: 2025/04/27 17:54:37 by eazard           ###   ########.fr       */
+/*   Updated: 2025/04/29 17:33:06 by eazard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	free_data(t_data *data, bool fatal)
 		free_linked_env_space(data, fatal);
 		free_parsing_space(data, fatal);
 		free_exec_space(data, fatal);
-		rl_clear_history();
+		if (fatal)
+			rl_clear_history();
 	}
 }
